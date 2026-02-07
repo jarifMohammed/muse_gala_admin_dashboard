02e4da2a34bd55b4d95f09fa201b5ad322734061
jarifMohammed <mohammedjarif@protonmail.com>
2026-02-07 14:54:20 +0600

    style: Update AuthHeader component to use local logo.svg for improved asset management

--- src/app/(auth)/sign-in/_components/AUthHeader.tsx
+++ src/app/(auth)/sign-in/_components/AUthHeader.tsx
@@ -5,14 +5,14 @@ const AuthHeader = () => {
     <div className="w-full flex flex-col items-center justify-center">
       {/* Logo */}
       <div className="mb-[35px] md:mb-[50px] lg:mb-[69px]">
-        <Image
-          src="https://files.edgestore.dev/2pgl62wxp0dbg019/Dev/_public/c07daf32-7d8a-4490-994e-00f3431e3de4.png"
-          alt="Logo"
-          width={94}
-          height={80}
-          unoptimized
-        />
-      </div>
+  <Image
+    src="/logo.svg"
+    alt="Logo"
+    width={200} 
+    height={200} 
+    unoptimized
+  />
+</div>
 
       {/* Heading */}
       <div className="text-center pb-[12px] md:pb-[15px]">
