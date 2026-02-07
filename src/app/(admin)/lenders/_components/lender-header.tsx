3f9208ef5aac94f2a512781afbc9eb63702cdbfb
jarifMohammed <mohammedjarif@protonmail.com>
2026-02-07 14:17:08 +0600

    refactor: Update LenderHeader component to remove download button and adjust title styling

--- src/app/(admin)/lenders/_components/lender-header.tsx
+++ src/app/(admin)/lenders/_components/lender-header.tsx
@@ -1,15 +1,11 @@
 import { InfoCard } from '@/components/cards/stat-card'
-import { Button } from '@/components/ui/button'
-import { Download } from 'lucide-react'
+
 
 const LenderHeader = () => {
   return (
     <div>
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-light tracking-[20%]">Manage Lenders</h1>
-        <Button>
-          Download Report <Download />
-        </Button>
+        <h1 className="text-2xl uppercase font-light tracking-[20%]">Manage Lenders</h1>
       </div>
 
       <div className="mt-[30px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 font-sans">
